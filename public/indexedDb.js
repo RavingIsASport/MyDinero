17922c55c40d108dd58090ae04fa2bf95787e46e
Gustavo Garcia <gustavogarcia42@yahoo.com>
2022-01-17 21:54:08 -0600

    fixed indexedDb and fixed server

--- public/indexedDb.js
+++ public/indexedDb.js
@@ -1,1 +1,28 @@
-const request = window.indexedDB.open("");
+const request = window.indexedDB.open("moneySpent", 1);
+let db;
+let tx;
+let store;
+let index;
+
+request.onupgradeneeded = function (e) {
+  let db = request.result;
+  store = db.createObjectStore("moneySpent", {
+    autoIncrement: true,
+  });
+};
+
+request.onerror = function (e) {
+  console.log("There was an error:" + e.target.errorCode);
+};
+
+request.onsuccess = function (e) {
+  db = request.result;
+  tx = db.transaction("moneySpent", "readwrite");
+  store = tx.objectStore("moneySpent");
+  db.onerror = function (e) {
+    console.log("Error" + e.target.errorCode);
+  };
+  tx.oncomplete = function () {
+    db.close();
+  };
+};
--- public/service-worker.js
+++ public/service-worker.js
@@ -2,12 +2,11 @@ const FILES_TO_CACHE = [
   "/",
   "/index.html",
   "/index.js",
-  "style.css",
+  "/indexedDb.js",
+  "/style.css",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
-  "/manifest.webmanifest",
-  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
-  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+  "manifest.webmanifest",
 ];
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE = "data-cache-v1";
